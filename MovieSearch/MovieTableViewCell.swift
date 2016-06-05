4f18a5db76f56ebf3a6c230d208c7ca91e7bbda2
K-scorpio <kevin.s.hartley@gmail.com>
2016-06-04 22:19:51 -0600

    first test before debugging

---
+++ MovieSearch/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Kevin Hartley on 6/4/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
